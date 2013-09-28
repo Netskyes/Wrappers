58d3192e77496c1467292d8aa28542cbd8d4da0a
Netskyes <alex@netskyes.com>
2013-09-28 19:43:35 +0200

    Create element.js

---
+++ element.js
@@ -0,0 +1,41 @@
+var Element = {
+
+	new : function( name, props ) {
+		if( name && typeof ( name ) === "string" ) {
+			
+			this.el = document.createElement( name );
+
+			if( props && typeof ( props ) === "object" ) {
+				var element = this.el;
+
+				Object.keys( props ).forEach(function( key ) {
+
+					if( typeof ( props[key] ) === "string" ) {
+
+						element.setAttribute(key, props[key]);
+					}
+					
+				});	
+				return this;
+			}
+			else return this.el;
+		}
+		else return false;
+	},
+
+	attach : function( name ) {
+		if( name && typeof ( name ) === "string" ) {
+		
+		var handle = document.querySelector( name );
+		if( handle !== null ) {
+			handle.appendChild(this.el);
+		}
+
+		} else {
+			if( document.body !== null ) {
+				document.body.appendChild(this.el);
+			}
+		} 
+	}
+
+};
